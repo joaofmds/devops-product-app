b49231d4eacd8b9e3297978de69ee7bd17668525
João Souza <ms.joao.felipe@gmail.com>
2025-07-26 15:30:05 -0300

    const API_URL = 'http://local.devops/api/'\;

--- frontend/src/components/ProductIndex.vue
+++ frontend/src/components/ProductIndex.vue
@@ -65,7 +65,7 @@
 
 const axios = require('axios').default;
 const moment = require('moment');
-const API_URL = 'http://localhost:8081/';
+const API_URL = 'http://local.devops/api/';
 
 export default {
   name: 'ProductIndex',
